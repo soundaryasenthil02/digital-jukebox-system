2ff43f653792cba13edfc138792ea04cd3dbd6e5
soundarya <soundaryas@soundaryas-MacBook-Pro.local>
2025-11-14 22:58:34 -0600

    feat: create database models for jukebox system

---
+++ music/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Artist, Album, Song, Playlist, PlaylistSong, PlayHistory
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('artist_id', 'artist_name', 'country')
+    search_fields = ('artist_name', 'country')
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('album_id', 'album_name', 'artist', 'release_year')
+    list_filter = ('release_year', 'artist')
+    search_fields = ('album_name', 'artist__artist_name')
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('song_id', 'title', 'album', 'genre', 'duration')
+    list_filter = ('genre', 'album')
+    search_fields = ('title', 'genre')
+
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('playlist_id', 'playlist_name', 'user', 'created_at')
+    list_filter = ('created_at', 'user')
+    search_fields = ('playlist_name', 'user__username')
+
+
+@admin.register(PlaylistSong)
+class PlaylistSongAdmin(admin.ModelAdmin):
+    list_display = ('playlist', 'song')
+    list_filter = ('playlist',)
+
+
+@admin.register(PlayHistory)
+class PlayHistoryAdmin(admin.ModelAdmin):
+    list_display = ('play_id', 'song', 'user', 'played_at')
+    list_filter = ('played_at', 'user')
+    search_fields = ('song__title', 'user__username')
---
+++ music/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Artist(models.Model):
+    artist_id = models.AutoField(primary_key=True)
+    artist_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        db_table = 'artists'
+    
+    def __str__(self):
+        return self.artist_name
+
+
+class Album(models.Model):
+    album_id = models.AutoField(primary_key=True)
+    album_name = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    release_year = models.IntegerField(null=True, blank=True)
+    
+    class Meta:
+        db_table = 'albums'
+    
+    def __str__(self):
+        return self.album_name
+
+
+class Song(models.Model):
+    song_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=150)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    duration = models.IntegerField(help_text="Duration in seconds")
+    genre = models.CharField(max_length=50, blank=True)
+    file_link = models.CharField(max_length=255, blank=True)
+    
+    class Meta:
+        db_table = 'songs'
+    
+    def __str__(self):
+        return self.title
+
+
+class Playlist(models.Model):
+    playlist_id = models.AutoField(primary_key=True)
+    playlist_name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'playlists'
+    
+    def __str__(self):
+        return self.playlist_name
+
+
+class PlaylistSong(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'playlist_songs'
+        unique_together = ('playlist', 'song')
+    
+    def __str__(self):
+        return f"{self.playlist.playlist_name} - {self.song.title}"
+
+
+class PlayHistory(models.Model):
+    play_id = models.AutoField(primary_key=True)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    played_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'play_history'
+    
+    def __str__(self):
+        return f"{self.user.username} played {self.song.title}"
